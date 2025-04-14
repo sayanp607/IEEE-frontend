030d933d4c11e1facdda49dddd2dc2e85a58d305
Sayan Paul <sayanp607@gmail.com>
2025-04-14 13:23:52 +0530

    Update Register.jsx

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -25,7 +25,7 @@ const Register = () => {
 
     try {
       const res = await axios.post(
-        "https://ieee-backend-gucf.onrender.com/api/auth/register",
+        "https://ieee-backend-8pft.onrender.com/api/auth/register",
         formData
       );
       localStorage.setItem("token", res.data.token);
